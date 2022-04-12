fa333f1d856c8b8430c3757ef8e2925bf2eb1f4d
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-04-12 18:33:40 -0300

    +Desafio11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,41 @@
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(phone) {
   // seu código aqui
+  if (phone.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (number of phone) {
+    if (number > 11 || number < 0) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+
+  let counter = 0;
+  let maxCounter = 0;
+
+  for (let i = 0; i < phone.length; i++) {
+    for (let j = 0; j < phone.length; j++) {
+      if (phone[i] === phone[j]) {
+        counter += 1;
+      }
+      if (counter > maxCounter) {
+        maxCounter = counter;
+      }
+      if (maxCounter > 2) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+    counter = 0;
+  }
+
+  return (
+    '(' +
+    phone.slice(0, 2).join('') +
+    ') ' +
+    phone.slice(2, 7).join('') +
+    '-' +
+    phone.slice(7, 11).join('')
+  );
 }
 
 // Desafio 12
